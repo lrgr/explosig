cd3bc37207ec68c380e78d27d73edf0cdcadee37
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-02-16 17:50:44 -0500

    added state machine

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
 import * as d3 from "d3";
+import StateMachine from "javascript-state-machine/dist/state-machine";
 
-const svg = d3.select('#main').append('svg')
-  .append('g');
+var svg = d3.select('#main').append('svg');
+
+var fsm = new StateMachine({
+  init: 'solid',
+  transitions: [
+    { name: 'melt', from: 'solid', to: 'liquid' },
+    { name: 'freeze', from: 'liquid', to: 'solid' }
+  ],
+  methods: {
+    onInit: function() { console.log('I initialized'); },
+    onMelt: function() { console.log('I melted'); },
+    onFreeze: function() { console.log('I froze'); }
+  }
+});
+
+
