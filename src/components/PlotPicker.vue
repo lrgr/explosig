5ad3100f0074c066fb6cdff7be9dc9b8405d5343
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-05-25 20:42:08 -0400

    Implemented plot addition by checkboxes, closes #38

--- src/components/PlotPicker.vue
+++ src/components/PlotPicker.vue
@@ -1,18 +1,33 @@
 <template>
-    <div>
-        <div class="plot-picker">
+    <div class="plot-options">
+        <div class="main-options">
             <h2>+ Plot</h2>
             <ul>
-                <li v-for="plotType in this.plotTypes" :key="plotType.type" @click="addPlot(plotType.type, plotType.title)">{{ plotType.title }}</li>
+                <li v-for="plotType in this.plotTypes" :key="plotType.type">
+                    <input type="checkbox" :value="plotType" :id="plotType.type" name="plot" v-model="plotsToAdd" />
+                    <label :for="plotType.type">{{ plotType.title }}</label>
+                </li>
+                <li>
+                    <input type="checkbox" disabled/>
+                    <label class="coming-soon">Manhattan Plot with Mutation Categories (Coming Soon)</label>
+                </li>
+                <li>
+                    <input type="checkbox" disabled/>
+                    <label class="coming-soon">Mutation Prevalence per Cancer Type (Coming Soon)</label>
+                </li>
+                <li>
+                    <input type="checkbox" disabled/>
+                    <label class="coming-soon">Signature Exposures per Cancer Type (Coming Soon)</label>
+                </li>
+                <li>
+                    <input type="checkbox" disabled/>
+                    <label class="coming-soon">Signatures (Coming Soon)</label>
+                </li>
             </ul>
         </div>
-        <h4>Coming Soon:</h4>
-        <ul>
-            <li>Signatures</li>
-            <li>Manhattan Plot with Mutation Contexts</li>
-            <li>Manhattan Plot with Mutation Types</li>
-            <li>Signature Exposure Box Plots per Cancer Type</li>
-        </ul>
+        <div class="actions-bar">
+            <span class="button button-secondary" v-on:click="addPlots()">Add</span>
+        </div>
     </div>
 </template>
 
@@ -23,27 +38,32 @@ export default {
     name: 'PlotPicker',
     data: function() {
         return {
+            plotsToAdd: [],
             plotTypes: [{
                 type: 'ExposuresPlot',
                 title: 'Signature Exposures with Clinical Data'
             }, {
-                type: 'SignatureGenomeBinsPlot',
-                title: 'Manhattan Plot with Signatures'
-            }, {
                 type: 'KataegisPlot',
                 title: 'Kataegis'
+            }, {
+                type: 'SignatureGenomeBinsPlot',
+                title: 'Manhattan Plot with Signatures'
             }]
         };
     },
     methods: {
-        addPlot: function(plotType, plotTitle, plotOptions = {}) {
-            this.$store.commit('addPlot', {
-                type: plotType,
-                id: getUUID(),
-                title: plotTitle,
-                options: plotOptions
-            });
-            this.$emit('closePlotPicker');
+        addPlots: function() {
+            let vm = this;
+            for(var i = 0; i < vm.plotsToAdd.length; i++) {
+                vm.$store.commit('addPlot', {
+                    type: vm.plotsToAdd[i].type,
+                    id: getUUID(),
+                    title: vm.plotsToAdd[i].title,
+                    options: {}
+                });
+            }
+            vm.$emit('closePlotPicker');
+            vm.plotsToAdd = [];
         }
     }
     
@@ -54,39 +74,61 @@ export default {
 
 @import './../variables.scss';
 
-.plot-picker {
-    height: 100%;
-    width: 100%;
-    box-sizing: border-box;
+.plot-options {
     display: flex;
     flex-direction: column;
-    padding: 1rem;
-    overflow-y: auto;
-    h2 {
-        flex-shrink: 0;
-        margin: 0.3rem;
-        display: inline-block;
-    }
-    ul {
-        box-sizing: border-box;
+    height: 100%;
+   .main-options {
         flex-grow: 1;
-        list-style-type: none;
-        padding-left:0;
-        list-style: none;
-        li {
-            &:hover {
-                color: $color-gray;
-            }
-            cursor: pointer;
-            margin: 1rem;
+        overflow-y: auto;
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+        h2 {
+            flex-shrink: 0;
+            margin: 0.3rem;
+            display: inline-block;
         }
-        
-        
-    }
-}
-h4 {
-    margin-left: 1rem;
-    margin-bottom: 0;
+        h4 {
+            margin-left: 1rem;
+            margin-bottom: 0;
+        }
+        ul {
+            display: inline-block;
+            box-sizing: border-box;
+            flex-grow: 1;
+            list-style-type: none;
+            padding-left:0;
+            list-style: none;
+            margin-top: 0;
+            li {
+                &:hover {
+                    color: $color-gray;
+                }
+                label {
+                    margin-left: 5px;
+                    cursor: pointer;
+                    &.coming-soon {
+                        color: silver;
+                    }
+                }
+                cursor: pointer;
+                margin: 1rem;
+            }
+        }
+        button.inline {
+            display: inline-block;
+        }
+   }
+
+   .actions-bar {
+        padding: 1rem;
+        .button {
+            float: right;
+            margin: 0rem 0rem 0rem 1rem;
+        }
+   }
+    
 }
 
 </style>
