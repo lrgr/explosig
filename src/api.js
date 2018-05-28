1e96da840936fbb60081ba05267e71261f18da29
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-05-28 13:09:58 -0400

    GCP backend

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 
 export default class API {
     //static api_base = 'http://localhost:8000/';
-    static api_base = 'https://afternoon-garden-99891.herokuapp.com/';
+    static api_base = 'http://35.199.44.26/';
 
     static fetchDataListing() {
         var url = API.api_base + "data-listing";
