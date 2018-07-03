726831592f095e9df7377658e60f39e60b174c09
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-07-02 21:15:21 -0400

    Updated about text and references

--- src/store/modules/bib.js
+++ src/store/modules/bib.js
@@ -4,16 +4,16 @@ const state = {
             code: "alexandrov2013signatures",
             title: "Signatures of mutational processes in human cancer",
             authors: [
-                "Alexandrov, Ludmil B", 
-                "Nik-Zainal, Serena",
-                "Wedge, David C",
-                "Aparicio, Samuel AJR",
-                "Behjati, Sam",
-                "Biankin, Andrew V",
-                "Bignell, Graham R",
-                "Bolli, Niccolo",
-                "Borg, Ake",
-                "Borresen-Dale, Anne-Lise",
+                "L. B. Alexandrov", 
+                "S. Nik-Zainal",
+                "D. C. Wedge",
+                "S. Aparicio",
+                "S. Behjati",
+                "A. V. Biankin",
+                "G. R. Bignell",
+                "N. Bolli",
+                "A. Borg",
+                "A. Borresen-Dale",
                 "et al"
             ],
             journal: "Nature",
@@ -27,9 +27,9 @@ const state = {
             code: "helleday2014mechanisms",
             title: "Mechanisms underlying mutational signatures in human cancers",
             authors: [
-                "Helleday, Thomas", 
-                "Eshtad, Saeed", 
-                "Nik-Zainal, Serena"
+                "T. Helleday", 
+                "S. Eshtad", 
+                "S. Nik-Zainal"
             ],
             journal: "Nature Reviews Genetics",
             volume: "15",
@@ -42,11 +42,11 @@ const state = {
             code: "alexandrov2013deciphering",
             title: "Deciphering signatures of mutational processes operative in human cancer",
             authors: [
-                "Alexandrov, Ludmil B",
-                "Nik-Zainal, Serena", 
-                "Wedge, David C", 
-                "Campbell, Peter J", 
-                "Stratton, Michael R"
+                "L. B. Alexandrov",
+                "S. Nik-Zainal", 
+                "D. C. Wedge", 
+                "P. J. Campbell", 
+                "M. R. Stratton"
             ],
             journal: "Cell Reports",
             volume: "3",
@@ -59,12 +59,12 @@ const state = {
             code: "zou2018validating",
             title: "Validating the concept of mutational signatures with isogenic cell models",
             authors: [
-                "Zou, Xueqing", 
-                "Owusu, Michel", 
-                "Harris, Rebecca", 
-                "Jackson, Stephen P", 
-                "Loizou, Joanna I", 
-                "Nik-Zainal, Serena"
+                "X. Zou", 
+                "M. Owusu", 
+                "R. Harris", 
+                "S. P Jackson", 
+                "J. I. Loizou", 
+                "S. Nik-Zainal"
             ],
             journal: "Nature Communications",
             volume: "9",
@@ -77,16 +77,16 @@ const state = {
             code: "nik2015genome",
             title: "The genome as a record of environmental exposure",
             authors: [
-                "Nik-Zainal, Serena", 
-                "Kucab, Jill E",
-                "Morganella, Sandro",
-                "Glodzik, Dominik",
-                "Alexandrov, Ludmil B",
-                "Arlt, Volker M",
-                "Weninger, Annette",
-                "Hollstein, Monica", 
-                "Stratton, Michael R",
-                "Phillips, David H"
+                "S. Nik-Zainal", 
+                "J. E. Kucab",
+                "S. Morganella",
+                "D. Glodzik",
+                "L. B. Alexandrov",
+                "V. M. Arlt",
+                "A. Weninger",
+                "M. Hollstein", 
+                "M. R. Stratton",
+                "D. H. Phillips"
             ],
             journal: "Mutagenesis",
             volume: "30",
@@ -94,6 +94,52 @@ const state = {
             pages: "763-770",
             year: "2015",
             publisher: "Oxford University Press UK"
+        },
+        {
+            code: "alexandrov2016mutational",
+            title: "Mutational signatures associated with tobacco smoking in human cancer",
+            authors: [
+                "L. B. Alexandrov", 
+                "Y. S. Ju",
+                "K. Haase",
+                "P. Van Loo",
+                "I. Martincorena",
+                "S. Nik-Zainal",
+                "Y.Totoki",
+                "A. Fujimoto", 
+                "H. Nakagawa",
+                "T. Shibata",
+                "et al"
+            ],
+            journal: "Science",
+            volume: "354",
+            number: "6312",
+            pages: "618-622",
+            year: "2016",
+            publisher: "American Association for the Advancement of Science"
+        },
+        {
+            code: "haradhvala2018distinct",
+            title: "Distinct mutational signatures characterize concurrent loss of polymerase proofreading and mismatch repair",
+            authors: [
+                "N. J. Haradhvala", 
+                "J. Kim",
+                "Y. E. Maruvka",
+                "P. Polak",
+                "D. Rosebrock",
+                "D. Livitz",
+                "J. M. Hess",
+                "I. Leshchiner", 
+                "A. Kamburov",
+                "K. W. Mouw",
+                "et al"
+            ],
+            journal: "Nature Communications",
+            volume: "9",
+            number: "1",
+            pages: "1746",
+            year: "2018",
+            publisher: "Nature Publishing Group"
         }
     ],
     bib: []
