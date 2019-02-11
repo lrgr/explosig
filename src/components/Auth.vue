c8229e5376e23105b5b59bbd0ea267c9b227731a
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2019-02-10 23:52:51 -0500

    SCSS import fix

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -57,6 +57,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import './../style/variables.scss';
 #auth {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
